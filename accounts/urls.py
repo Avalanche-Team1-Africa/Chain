755261389424f2ae6436788095c53261e1136837
kelvin wepo <kelvinwepo7710@gmail.com>
2025-04-19 06:33:02 +0300

    this commit modifies  accounts/urls.py.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,20 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
-    
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    # path('logout/', auth_views.LogoutView.as_view(next_page='accounts:login'), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('verify-email/', views.VerifyEmailView.as_view(), name='verify_email'),
     path('profile-setup/', views.ProfileSetupView.as_view(), name='profile_setup'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('ngo/profile/', views.NGOProfileView.as_view(), name='ngo_profile'),
+    path('settings/', views.settings_view, name='settings'),  # Add this line
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('ngo/dashboard/', views.ngo_dashboard, name='ngo_dashboard'),
+    path('lawyer/dashboard/', views.lawyer_dashboard, name='lawyer_dashboard'),
+    path('donor/dashboard/', views.donor_dashboard, name='donor_dashboard'),
 ]
