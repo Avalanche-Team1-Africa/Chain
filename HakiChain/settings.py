85e63232dbcc896c6d70cc23ea5fcd5f75727945
kelvin wepo <kelvinwepo7710@gmail.com>
2025-05-05 07:06:11 +0300

    deployment

--- HakiChain/settings.py
+++ HakiChain/settings.py
@@ -31,8 +31,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'chain-azf7.onrender.com',  
+]
 
 # Application definition
 
