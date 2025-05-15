49bfca27e8ee8d76e64816fd11d337c7e38f3660
kelvin wepo <kelvinwepo7710@gmail.com>
2025-05-15 19:50:38 +0300

    this commit modifies forms.py

--- cases/forms.py
+++ cases/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from .models import Case, CaseDocument, CaseUpdate, LawyerApplication, SuccessStory
+from .models import Case, CaseDocument, CaseUpdate, LawyerApplication, SuccessStory, CaseEvent, CaseMessage, DocumentTemplate
 from accounts.models import LawyerProfile
 from multiupload.fields import MultiFileField
+
 
 class CaseForm(forms.ModelForm):
     class Meta:
@@ -12,11 +13,20 @@ class CaseForm(forms.ModelForm):
             'description': forms.Textarea(attrs={'rows': 5}),
         }
 
-
 class CaseDocumentForm(forms.ModelForm):
+    """Form for uploading case documents."""
+
     class Meta:
         model = CaseDocument
-        fields = ['title', 'file']
+        fields = ['name', 'file']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'file': forms.FileInput(attrs={'class': 'form-control'}),
+        }
+        help_texts = {
+            'name': "Give a name or title to this document",
+            'file': "Upload the file (PDF, Word, etc.)",
+        }
 
 
 class CaseUpdateForm(forms.ModelForm):
@@ -68,6 +78,8 @@ class CaseCompletionForm(forms.Form):
         required=False,
         help_text="Upload up to 5 final documents (5MB max each)."
     )
+
+
 class SuccessStoryForm(forms.ModelForm):
     class Meta:
         model = SuccessStory
@@ -77,4 +89,134 @@ class SuccessStoryForm(forms.ModelForm):
         }
         help_texts = {
             'is_public': "If checked, this success story will be visible on your public profile."
+        }
+
+
+class CaseEventForm(forms.ModelForm):
+    """Form for creating and editing case events."""
+
+    class Meta:
+        model = CaseEvent
+        fields = [
+            'case', 'title', 'description', 'event_type',
+            'start_time', 'end_time', 'location'
+        ]
+        widgets = {
+            'start_time': forms.DateTimeInput(
+                attrs={'type': 'datetime-local'},
+                format='%Y-%m-%dT%H:%M'
+            ),
+            'end_time': forms.DateTimeInput(
+                attrs={'type': 'datetime-local'},
+                format='%Y-%m-%dT%H:%M'
+            ),
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+        # Format initial datetime values
+        if self.instance.start_time:
+            self.initial['start_time'] = self.instance.start_time.strftime('%Y-%m-%dT%H:%M')
+        if self.instance.end_time:
+            self.initial['end_time'] = self.instance.end_time.strftime('%Y-%m-%dT%H:%M')
+
+        # Limit case choices based on user role
+        if user:
+            if user.role == 'NGO':
+                self.fields['case'].queryset = Case.objects.filter(ngo=user)
+            elif user.role == 'LAWYER' and hasattr(user, 'lawyer_profile'):
+                self.fields['case'].queryset = Case.objects.filter(
+                    assigned_lawyer=user.lawyer_profile
+                )
+
+
+class CaseMessageForm(forms.ModelForm):
+    attachments = MultiFileField(
+        min_num=1,
+        max_num=5,
+        max_file_size=1024 * 1024 * 5,  # 5 MB
+        required=False,
+        label="Attachments",
+        help_text="Upload up to 5 files (max 5MB each)."
+    )
+
+    class Meta:
+        model = CaseMessage
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Type your message here...'}),
+        }
+
+class DocumentGenerationForm(forms.Form):
+    """
+    Dynamic form for generating documents from templates.
+    Fields are created based on the required_fields of the template.
+    """
+    document_name = forms.CharField(
+        max_length=255,
+        help_text="Name for the generated document"
+    )
+
+    def __init__(self, *args, **kwargs):
+        template = kwargs.pop('template', None)
+        super().__init__(*args, **kwargs)
+
+        if template:
+            # Add fields based on template required_fields
+            for field_info in template.required_fields:
+                field_name = field_info.get('name')
+                field_type = field_info.get('type', 'text')
+                field_label = field_info.get('label', field_name.replace('_', ' ').title())
+                field_required = field_info.get('required', True)
+
+                if field_type == 'text':
+                    self.fields[field_name] = forms.CharField(
+                        label=field_label,
+                        required=field_required,
+                        max_length=255
+                    )
+                elif field_type == 'textarea':
+                    self.fields[field_name] = forms.CharField(
+                        label=field_label,
+                        required=field_required,
+                        widget=forms.Textarea(attrs={'rows': 4})
+                    )
+                elif field_type == 'date':
+                    self.fields[field_name] = forms.DateField(
+                        label=field_label,
+                        required=field_required,
+                        widget=forms.DateInput(attrs={'type': 'date'})
+                    )
+                elif field_type == 'number':
+                    self.fields[field_name] = forms.IntegerField(
+                        label=field_label,
+                        required=field_required
+                    )
+                elif field_type == 'checkbox':
+                    self.fields[field_name] = forms.BooleanField(
+                        label=field_label,
+                        required=False
+                    )
+                elif field_type == 'select':
+                    choices = [(c, c) for c in field_info.get('choices', [])]
+                    self.fields[field_name] = forms.ChoiceField(
+                        label=field_label,
+                        required=field_required,
+                        choices=choices
+                    )
+
+class DocumentTemplateForm(forms.ModelForm):
+    """Form for creating and editing document templates."""
+
+    class Meta:
+        model = DocumentTemplate
+        fields = [
+            'name', 'description', 'template_file',
+            'available_to_ngo', 'available_to_lawyer'
+        ]
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3}),
         }
