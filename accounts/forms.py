10a1e6470d1e639eca9a1252d286b01e193e4054
kelvin wepo <kelvinwepo7710@gmail.com>
2025-06-11 12:48:06 +0300

    modification

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User, NGOProfile, LawyerProfile, DonorProfile
-
 class UserRegistrationForm(UserCreationForm):
+    phone_number = forms.CharField(
+        label='Phone Number',
+        help_text='Enter your phone number starting with +254. Example: +254712345678'
+    )
+
     class Meta:
         model = User
         fields = ('email', 'role', 'phone_number', 'password1', 'password2')
-
 class NGOProfileForm(forms.ModelForm):
     class Meta:
         model = NGOProfile
